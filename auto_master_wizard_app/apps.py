13c101683e1bad6e8ac946b6b63a41e58a370d86
polak <polak.barel@gmail.com>
2023-07-02 19:18:23 +0300

    models

---
+++ auto_master_wizard_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AutoMasterWizardAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'auto_master_wizard_app'
---
+++ auto_master_wizard_app/models.py
@@ -0,0 +1,72 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Manufacturer(models.Model):
+
+    manufacturer_name = models.CharField(max_length=128, unique=True)
+
+    class Meta:
+        db_table = 'manufacturers'
+
+
+class Model(models.Model):
+
+    model_name = models.CharField(max_length=128, unique=True)
+    manufacturer = models.ForeignKey('Manufacturer', on_delete=models.RESTRICT, related_name='models')
+
+    class Meta:
+        db_table = 'models'
+
+
+class SubModel(models.Model):
+
+    sub_model_code = models.CharField(max_length=128, unique=True)
+    model = models.ForeignKey('Model', on_delete=models.RESTRICT, related_name='sub_models')
+
+    class Meta:
+        db_table = 'sub_models'
+
+
+class Trim(models.Model):
+
+    trim_code = models.CharField(max_length=128, unique=True)
+    sub_model = models.ForeignKey('SubModel', on_delete=models.RESTRICT, related_name='trims')
+    users = models.ManyToManyField(User, through='Favourite', related_name='favourite_trims')
+
+    class Meta:
+        db_table = 'trims'
+
+
+class Favourite(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.RESTRICT, related_name='favourites')
+    trim = models.ForeignKey('Trim', on_delete=models.RESTRICT, related_name='favourites')
+
+    class Meta:
+        db_table = 'favourites'
+
+
+class Profile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+
+    class Meta:
+        db_table = 'profiles'
+
+
+class Content(models.Model):
+
+    title = models.CharField()
+    uploaded_by = models.ForeignKey(User, on_delete=models.RESTRICT, related_name='contents', null=True)
+    trim = models.ForeignKey('Trim', on_delete=models.RESTRICT, related_name='contents')
+    content_type = models.CharField()
+    source = models.CharField(max_length=256, null=True)
+    url = models.CharField()
+
+    class Meta:
+        db_table = 'contents'
+
+
